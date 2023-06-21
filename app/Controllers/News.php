eca4fe797fe3099d9695f0e217fcbe8fbb8b0ca4
LINUX <linux@localhost.localdomain>
2023-06-21 22:49:14 +0300

    Display The News

--- app/Controllers/News.php
+++ app/Controllers/News.php
@@ -3,18 +3,38 @@
 namespace App\Controllers;
 
 use App\Models\NewsModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class News extends BaseController
 {
     public function index()
     {
         $model = model(NewsModel::class);
-        $data['news'] = $model->getNews();
+
+        $data = [
+            'news' => $model->getNews(),
+            'title' => 'News archieve',
+        ];
+
+        return view('templates/header', $data)
+            . view('news/index')
+            . view('templates/footer');
     }
 
     public function view($slug = null)
     {
         $model = model(NewsModel::class);
+
         $data['news'] = model->getNews($slug);
+
+        if (empty($data['news'])) {
+            throw new PageNotFoundExeption('Cannot find the news item: ' . $slug);
+        }
+
+        $data['title'] = $data['news']['title'];
+
+        return view('templates/header', $data)
+            . view('news/view')
+            . view('templates/footer');
     }
 }
